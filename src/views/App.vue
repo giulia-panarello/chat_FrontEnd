90cc33e6cf5dd24b7389cc9df74ddb3e10617f38
giulia-panarello <162906740+giulia-panarello@users.noreply.github.com>
2024-06-29 01:32:45 +0200

    Add files via upload

---
+++ src/views/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+      
+      <router-view />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+    methods: {
+      goToChat() {
+        this.$router.push({ name: 'chat-gruppo' });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* Stili globali dell'applicazione */
+  
+  </style>
+  
