e759cc2943007bb52467de91cb87958c5b5f5862
giulia-panarello <162906740+giulia-panarello@users.noreply.github.com>
2024-06-29 09:57:23 +0200

    ListaPartecipanti.vue

--- src/views/ListaPartecipanti.vue
+++ src/views/ListaPartecipanti.vue
@@ -1,17 +1,1 @@
-<!-- ListaPartecipanti.vue -->
-<template>
-  <div>
-    <h1>Lista dei Partecipanti</h1>
-    <!-- Contenuto della lista dei partecipanti -->
-  </div>
-</template>
 
-<script>
-export default {
-  // Opzionale: logica del componente se necessario
-}
-</script>
-
-<style scoped>
-/* Stili del componente, se necessario */
-</style>
