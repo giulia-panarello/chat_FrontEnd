7518cc4df8a70f7d3ae2ed4d618024bb3805efe3
giulia-panarello <162906740+giulia-panarello@users.noreply.github.com>
2024-07-13 21:25:45 +0200

    router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import ChatGruppo from '@/views/ChatGruppo.vue';
 import ListaPartecipanti from '@/views/ListaPartecipanti.vue';
+import ChatPrivata from '@/views/ChatPrivata.vue';
 
 const routes = [
   { path: '/', component: ChatGruppo, name: 'chat-gruppo' },
   { path: '/lista-partecipanti', component: ListaPartecipanti, name: 'lista-partecipanti' },
+  { path: '/chat-privata', component: ChatPrivata, name: 'chat-privata' },
 ];
 
 const router = createRouter({
