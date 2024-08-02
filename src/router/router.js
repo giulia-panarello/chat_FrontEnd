40017d876bb415d3c5a9d8a09385af3eb2ef966c
giulia-panarello <162906740+giulia-panarello@users.noreply.github.com>
2024-08-02 18:58:49 +0200

    router.js

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,16 @@ import ChatGruppo from '@/views/ChatGruppo.vue';
 import ListaPartecipanti from '@/views/ListaPartecipanti.vue';
 import ChatPrivata from '@/views/ChatPrivata.vue';
 import ListaChat from '@/views/ListaChat.vue';
+import CreaGruppo from '@/views/CreaGruppo.vue';
+import SelezionaUtente from '@/views/SelezionaUtente.vue'
 
 const routes = [
   { path: '/', component: ListaChat, name: 'lista-chat' },
-  { path: '/chat-gruppo/:id', component: ChatGruppo, name: 'chat-gruppo' },
+  { path: '/chat-gruppo', component: ChatGruppo, name: 'chat-gruppo' },
   { path: '/lista-partecipanti', component: ListaPartecipanti, name: 'lista-partecipanti' },
-  { path: '/chat-privata/:id/:userName', component: ChatPrivata, name: 'chat-privata' },
+  { path: '/chat-privata', component: ChatPrivata, name: 'chat-privata' },
+  { path: '/crea-nuovo-gruppo', name: 'crea-nuovo-gruppo', component: CreaGruppo },
+  { path: '/seleziona-utente', name: 'seleziona-utente', component: SelezionaUtente },
 ];
 
 const router = createRouter({
