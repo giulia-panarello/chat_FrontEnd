1b0d823c1f2b14fdb95092dc95d789be01ceb971
edoardoSangiorgi <163001347+edoardoSangiorgi@users.noreply.github.com>
2024-08-15 18:50:26 +0200

    Cambiamento route e parametri

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,15 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import ChatGruppo from '@/views/ChatGruppo.vue';
-import ListaPartecipanti from '@/views/ListaPartecipanti.vue';
 import ChatPrivata from '@/views/ChatPrivata.vue';
 import ListaChat from '@/views/ListaChat.vue';
 import CreaGruppo from '@/views/CreaGruppo.vue';
 import SelezionaUtente from '@/views/SelezionaUtente.vue'
+import ChatInfo from "@/views/ChatInfo.vue";
 
 const routes = [
   { path: '/', component: ListaChat, name: 'lista-chat' },
-  { path: '/chat-gruppo', component: ChatGruppo, name: 'chat-gruppo' },
-  { path: '/lista-partecipanti', component: ListaPartecipanti, name: 'lista-partecipanti' },
+  { path: '/chat-gruppo/:chatName', component: ChatGruppo, name: 'chat-gruppo' },
+  { path: '/chat-gruppo/:chatName/chat-info', component: ChatInfo, name: 'lista-partecipanti' },
   { path: '/chat-privata', component: ChatPrivata, name: 'chat-privata' },
   { path: '/crea-nuovo-gruppo', name: 'crea-nuovo-gruppo', component: CreaGruppo },
   { path: '/seleziona-utente', name: 'seleziona-utente', component: SelezionaUtente },
